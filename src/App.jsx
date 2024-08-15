2d2ea7655064bd0acad154b71769fe6d1bf763ac
MD. ZULFIKER HOSSAIN <zisan19393871@gmail.com>
2024-08-15 09:47:39 +0600

    Project SetUp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DashboardPage from "./pages/DashboardPage";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Render the DashboardPage component */}
+      <DashboardPage />
+    </div>
+  );
+}
+
+export default App;
